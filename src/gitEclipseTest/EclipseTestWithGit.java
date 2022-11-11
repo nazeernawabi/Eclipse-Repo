875a477c2dd83b85b91975ef916634f6b2efc60e
Nazeer Nawabi <nazeernawabi@yahoo.com>
2022-11-10 18:41:44 -0800

    yes

--- src/gitEclipseTest/EclipseTestWithGit.java
+++ src/gitEclipseTest/EclipseTestWithGit.java
@@ -7,7 +7,7 @@ public class EclipseTestWithGit {
 		System.out.println("Hello world");
 		System.out.println("Hello world");
 		System.out.println("Hello world");
-		System.out.println("Hello world");
+		System.out.println("Hello ");
 		
 	}
 	
